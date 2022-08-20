922c21cc37b58e7a1bfb411f7b9d0e12d14953cb
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-20 06:03:16 -0300

    implementing token system for authentication

--- projeto/backend/core/views.py
+++ projeto/backend/core/views.py
@@ -1,6 +1,6 @@
 from .models import List, Item
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 from .serializers import ListSerializer, ItemSerializer
 
 
@@ -9,6 +9,7 @@ class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
@@ -16,4 +17,5 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
