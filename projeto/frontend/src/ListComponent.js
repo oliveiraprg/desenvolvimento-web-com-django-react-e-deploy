2c5c8b22bf8ff316b733ebc8632a2da6cf23eb2c
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-21 08:16:56 -0300

    creating list component

---
+++ projeto/frontend/src/ListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ export default function ListComponent(){
+    return(
+        <div>
+            <ul>
+                <li>Primeiro Item</li>
+                <li>Segundo Item</li>
+                <li>Terceiro Item</li>
+            </ul>
+        </div>
+    )
+ }
