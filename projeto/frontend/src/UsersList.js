81d6399e047e5db47a9ead993f6fb5e761013e93
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-22 07:37:28 -0300

    class component

--- projeto/frontend/src/UsersList.js
+++ projeto/frontend/src/UsersList.js
@@ -2,12 +2,15 @@ import React from "react";
 import ListComponent from "./ListComponent";
 
 
-export default function UsersLists(){
-    return (
-        <div>
-            <ListComponent listName={'Lista 1'}/>
-            <ListComponent listName={'Lista 2'}/>
-            <ListComponent listName={'Lista 3'}/>
-        </div>
-    )
+export default class UsersLists extends React.Component (){
+    render(){
+    	return (
+	        <div>
+	            <ListComponent listName={'Lista 1'}/>
+	            <ListComponent listName={'Lista 2'}/>
+	            <ListComponent listName={'Lista 3'}/>
+	        </div>
+	    )
+    }
+    
 }
