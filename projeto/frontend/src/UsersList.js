0fbae8455bb66c3d6716c3f002f76ea6ee73a9fb
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-08-22 08:12:47 -0300

    backend communication frontend by authentication

--- projeto/frontend/src/UsersList.js
+++ projeto/frontend/src/UsersList.js
@@ -2,8 +2,25 @@ import React from "react";
 import ListComponent from "./ListComponent";
 
 
-export default class UsersLists extends React.Component (){
-    render(){
+export default class UsersLists extends React.Component{
+	state = {lists: null, loading:true}
+	
+	async componentDidMount(){
+		const config = {
+			headers:{
+				'Content-Type': 'application/json'
+			}
+		}
+		config.headers['Authorization'] = 'Token 4fb66f537557b52bc3b77b1dcbce15671c0d0fe4'
+
+		var url = 'http://localhost:8000/list/';
+		const response = await fetch(url, config)
+		const data = await response.json();
+		console.log(data)
+		this.setState({lists:data, loading:false})
+	}
+	
+	render(){
     	return (
 	        <div>
 	            <ListComponent listName={'Lista 1'}/>
